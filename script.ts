e56128ee7f653c0a0c056124587ef9206fbf6d91
david cruz hernandez <77642460+David-cruz7@users.noreply.github.com>
2022-01-30 16:33:16 -0600

    Add files via upload

---
+++ script.ts
@@ -0,0 +1,117 @@
+//canvas 1
+const canvas:any = document.getElementById('canvas');
+const ctx:any = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+//canvas 2
+const canvasbg:any = document.getElementById('canvasbg');
+const ctxbg:any = canvasbg.getContext('2d');
+canvasbg.width = window.innerWidth;
+canvasbg.height = window.innerHeight;
+
+let Bubbles:any = [];
+let bgBubbles:any = [];
+
+function addBubble(){
+    Bubbles.push(new Bubble('rgb(255,194,194)', 1.8));
+}
+
+function addBgBubble(){
+    bgBubbles.push(new Bubble('rgb(255,255,255)', 2.5));
+}
+
+class Bubble {
+    public radius:number;
+    public life:any;
+    public x:number;
+    public y:number;
+    public vy:number;
+    public vr:number;
+    public vx:number;
+    public color:number;
+    constructor(color:any , ySpeed:any) {
+        this.radius = (Math.random() * 150) + 30;
+        this.life = true;
+        this.x = (Math.random() * window.innerWidth);
+        this.y = (Math.random() * 20) + window.innerHeight + this.radius;
+        this.vy = ((Math.random() * 0.0002) + 0.001) + ySpeed;
+        this.vr = 0;
+        this.vx = (Math.random() * 4) - 2;
+        this.color = color;
+        
+    }
+
+    update() {
+        this.vy += 0.0001;
+        this.vr += 0.02;
+        this.y -= this.vy;
+        this.x += this.vx;
+        if(this.radius > 1){
+            this.radius -= this.vr;
+        }
+        if(this.radius <= 1){
+            this.life = false;
+        }
+    }
+    draw(currentCanvas:any){
+        currentCanvas.beginPath();
+        currentCanvas.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        currentCanvas.fillStyle = this.color;
+        currentCanvas.fill();
+    }
+}
+
+function handleBubbles () {
+    for(let i= Bubbles.length -1; i >= 0; i--){
+        Bubbles[i].update();
+        if(!Bubbles[i].life){
+            Bubbles.splice(i, 1);
+        }
+    }
+    for(let i= bgBubbles.length -1; i >= 0; i--){
+        bgBubbles[i].update();
+        if(!bgBubbles[i].life){
+            bgBubbles.splice(i, 1);
+        }
+    }
+    if(Bubbles.length < (window.innerWidth/4)){
+        addBubble();
+    }
+    if(bgBubbles.length < (window.innerWidth/12)){
+        addBgBubble();
+    }
+}
+
+function animate(){
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    ctxbg.clearRect(0,0,canvas.width, canvas.height);
+
+    handleBubbles();
+
+    for(let i = bgBubbles.length - 1; i >=0; i--){
+        bgBubbles[i].draw(ctxbg);
+    }
+
+    for(let i = Bubbles.length - 1; i >=0; i--){
+        Bubbles[i].draw(ctx);
+    }
+    requestAnimationFrame(animate);
+}
+
+window.addEventListener('load', animate);
+
+window.addEventListener('resize', function(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvasbg.width = window.innerWidth;
+    canvasbg.height = window.innerHeight;
+    let Bubbles:any = [];
+    let bgBubbles:any = [];
+
+});
+
+setInterval(function(){
+    console.log(bgBubbles.length)
+},1000);
